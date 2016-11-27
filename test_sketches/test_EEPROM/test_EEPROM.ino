4eedabab36bf5e36976014b06e92ecc91f11e898
dmashine <cpsoft@gmail.com>
2016-11-27 23:54:49 +0300

    upd

--- test_sketches/test_EEPROM/test_EEPROM.ino
+++ test_sketches/test_EEPROM/test_EEPROM.ino
@@ -1,35 +1,29 @@
 #include <EEPROM.h>
 #define MAX_I 3
 
-int led[MAX_I] = {2, 3, 4};
-int btn[MAX_I] = {8, 9, 10};
+int led[MAX_I] = {2, 3, 4};  // Светодиоды подключены к 2, 3, 4 пинам
+int btn[MAX_I] = {8, 9, 10}; // Кнопки подключены к 8, 9, 10
 
 // the setup routine runs once when you press reset:
 void setup() {
-  for(int i=0; i< MAX_I; i++)
+  // инициализация светодиодов и кнопок
+  for (int i = 0; i < MAX_I; i++)
     pinMode(led[i], OUTPUT);
-  for(int i=0; i< MAX_I; i++)
+  for (int i = 0; i < MAX_I; i++)
     pinMode(btn[i], INPUT);
-  sig();
+  sig(); // При старте вызовем функциию показа состояния
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  for(int i=0; i< MAX_I; i++)
-  { if(digitalRead(btn[i]) == HIGH){
-      EEPROM[i] = !EEPROM[i];
-  }}
-  sig();
-
-}
-void sig(void){
-  for(int i=0; i< MAX_I; i++)
-  {
-    if(EEPROM[i] == HIGH){
-      digitalWrite(led[i], HIGH);
-    }
-    else{
-      digitalWrite(led[i], LOW);
-    }
+  for (int i = 0; i < MAX_I; i++)
+  { if (digitalRead(btn[i]) == HIGH) // Если кнопка нажата
+      EEPROM[i] = !EEPROM[i];        // Переключим значение в памяти
   }
+  sig();
+}
+void sig(void) {
+  // Зажигаем и гасим светодиоды соответственно значению в памяти
+  for (int i = 0; i < MAX_I; i++)
+    digitalWrite(led[i], EEPROM[i]);
 }
